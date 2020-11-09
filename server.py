2946a02a15fbd95d38649eebd83e3e5830af9aac
SharmilaS22 <52574006+SharmilaS22@users.noreply.github.com>
2020-11-09 16:22:02 +0530

    changes in api routing

--- server.py
+++ server.py
@@ -11,29 +11,46 @@ def read_root():
     else:
         return {'route': '/', 'method': 'delete'}
 
-@app.route('/:username', methods=['GET', 'POST', 'DELETE'])
-def read_user():
+@app.route('/user/<username>', methods=['GET', 'POST', 'DELETE'])
+def read_user(username):
     if(request.method == 'GET'):
-        return {'route': '/:username', 'method': 'get'}
+        if('id' in request.args):
+            request_date = request.args['id']
+            return {'id': str(request_date)}
+        else:
+            return {'route': '/:username', 'method': 'get'}
     elif( request.method == 'POST' ):
-        return {'route': '/:username', 'method': 'post'}
-    else:
-        return {'route': '/:username', 'method': 'delete'}
-
-@app.route('/:username/update', methods=['PUT', 'PATCH'])
-def update_user():
-    if(request.method == 'PUT'):
-        return {'route': '/:username/update', 'method': 'put'}
-    else:
-        return {'route': '/:username/update', 'method': 'patch'}
-
-@app.route('/:username/date/:date', methods=['GET', 'DELETE', 'PUT', 'PATCH'])
-def read_journal():
-    if(request.method == 'GET'):
-        return {'route': '/:username/date/:date', 'method': 'get'}
+        if ('id' not in request.args):
+            return {'route': '/:username', 'method': 'post'}
     elif( request.method == 'DELETE' ):
-        return {'route': '/:username/date/:date', 'method': 'delete'}
+        if('id' in request.args):
+            request_date = request.args['id']
+            return {'id': str(request_date)}
+        else:
+            return {'route': '/:username', 'method': 'delete'}
     elif( request.method == 'PUT' ):
-        return {'route': '/:username/date/:date', 'method': 'put'}
-    else:
-        return {'route': '/:username/date/:date', 'method': 'patch'}
+        if('id' in request.args):
+            return {'function': 'update a journal'}
+    elif(request.method == 'PATCH'):
+        if('id' in request.args):
+            return {'function': 'update a journal - patch'}
+
+
+# @app.route('/user/<username>/update', methods=['PUT', 'PATCH'])
+# def update_user(username):
+#     if(request.method == 'PUT'):
+#         return {'route': '/:username/update', 'method': 'put'}
+#     else:
+#         return {'route': '/:username/update', 'method': 'patch'}
+
+
+# @app.route('/user/<username>', methods=['GET', 'DELETE', 'PUT', 'PATCH'])
+# def read_journal(username, date):
+#     if(request.method == 'GET'):
+#         return {'route': '/:username/date/:date', 'method': 'get'}
+#     elif( request.method == 'DELETE' ):
+#         return {'route': '/:username/date/:date', 'method': 'delete'}
+#     elif( request.method == 'PUT' ):
+#         return {'route': '/:username/date/:date', 'method': 'put'}
+#     else:
+#         return {'route': '/:username/date/:date', 'method': 'patch'}
