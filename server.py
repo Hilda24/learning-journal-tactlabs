6f037cd05558791f77c6667ad369adc0b9933187
SharmilaS22 <52574006+SharmilaS22@users.noreply.github.com>
2020-11-09 15:54:31 +0530

    routing in flask

--- server.py
+++ server.py
@@ -1,5 +1,39 @@
-from flask import flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST', 'DELETE'])
+def read_root():
+    if( request.method == 'GET' ):
+        return {'route': '/', 'method': 'get'}
+    elif( request.method == 'POST' ):
+        return {'route': '/', 'method': 'post'}
+    else:
+        return {'route': '/', 'method': 'delete'}
+
+@app.route('/:username', methods=['GET', 'POST', 'DELETE'])
+def read_user():
+    if(request.method == 'GET'):
+        return {'route': '/:username', 'method': 'get'}
+    elif( request.method == 'POST' ):
+        return {'route': '/:username', 'method': 'post'}
+    else:
+        return {'route': '/:username', 'method': 'delete'}
+
+@app.route('/:username/update', methods=['PUT', 'PATCH'])
+def update_user():
+    if(request.method == 'PUT'):
+        return {'route': '/:username/update', 'method': 'put'}
+    else:
+        return {'route': '/:username/update', 'method': 'patch'}
+
+@app.route('/:username/date/:date', methods=['GET', 'DELETE', 'PUT', 'PATCH'])
+def read_journal():
+    if(request.method == 'GET'):
+        return {'route': '/:username/date/:date', 'method': 'get'}
+    elif( request.method == 'DELETE' ):
+        return {'route': '/:username/date/:date', 'method': 'delete'}
+    elif( request.method == 'PUT' ):
+        return {'route': '/:username/date/:date', 'method': 'put'}
+    else:
+        return {'route': '/:username/date/:date', 'method': 'patch'}
