d951afbf99c10a6fb254808c7ec9457a0819086b
Hilda24 <godlin.hilda@gmail.com>
2020-11-10 11:30:46 +0530

    MongoDB connect

---
+++ run.py
@@ -0,0 +1,26 @@
+import os
+import json
+import pymongo
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+client = pymongo.MongoClient("mongodb+srv://Godlin_hilda:1234@cluster0.mvqfv.mongodb.net/<dbname>?retryWrites=true&w=majority")
+db = client['test']
+collection = db['test']
+@app.route("/", methods=['POST'])
+def insert_document():
+    req_data = request.get_json()
+    collection.insert_one(req_data).inserted_id
+    return ('', 204)
+@app.route('/')
+def get():
+    documents = collection.find()
+    response = []
+    for document in documents:
+        document['_id'] = str(document['_id'])
+        response.append(document)
+    return json.dumps(response)
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
