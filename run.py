378bac29966820a0e03d950d2ba3084b1595fdcf
Hilda24 <godlin.hilda@gmail.com>
2020-11-10 18:28:49 +0530

    Delete

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ import json
 import pymongo
 from flask import Flask
 from flask import request
+from bson.objectid import ObjectId
 app = Flask(__name__)
 
-client = pymongo.MongoClient("mongodb+srv://Godlin_hilda:1234@cluster0.mvqfv.mongodb.net/<dbname>?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://Godlin_hilda:1234@cluster0.mvqfv.mongodb.net/test?retryWrites=true&w=majority")
 db = client['test']
 collection = db['test']
 @app.route("/", methods=['POST'])
@@ -21,6 +22,11 @@ def get():
         document['_id'] = str(document['_id'])
         response.append(document)
     return json.dumps(response)
+@app.route("/",methods=['DELETE'])
+def delete():
+    collection.remove({})
+    return ("",204)
+    
 if __name__ == '__main__':
     app.debug=True
     app.run()
